28b321b89e15265bb920f53f3f859339a0685ee7
User <User@DESKTOP-RBMPGAB>
2023-09-11 11:27:24 +0700

    Добавьте файлы проекта.

---
+++ ConsoleAppJacob/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+
+public class PerformanceTester
+{
+    public static void Main()
+    {
+        Console.WriteLine("Запуск тестирования производительности...");
+
+        double cpuPerformance = TestCPU();
+        Console.WriteLine($"Производительность процессора: {cpuPerformance}%");
+
+        double memoryPerformance = TestMemory();
+        Console.WriteLine($"Производительность памяти: {memoryPerformance}%");
+
+        double diskPerformance = TestDisk();
+        Console.WriteLine($"Производительность дисков: {diskPerformance}%");
+
+        Console.WriteLine("Тестирование производительности завершено.");
+    }
+
+    private static double TestCPU()
+    {
+
+        double cpuPerformance = 50.0;
+        return cpuPerformance;
+    }
+
+    private static double TestMemory()
+    {
+        double memoryPerformance = 80.0;
+        return memoryPerformance;
+    }
+
+    private static double TestDisk()
+    {
+
+        double diskPerformance = 70.0;
+        return diskPerformance;
+    }
+}
+
+//using System;
+
+//public class UpdateChecker
+//{
+//    public static void Main()
+//    {
+//        // Проверка наличия и установка обновлений операционной системы
+//        CheckAndInstallOSUpdates();
+
+//        // Проверка наличия и установка обновлений приложений
+//        CheckAndInstallAppUpdates();
+//    }
+
+//    private static void CheckAndInstallOSUpdates()
+//    {
+//        Console.WriteLine("Проверка наличия и установка обновлений операционной системы...");
+
+//        // Код для проверки наличия и установки обновлений операционной системы
+//        // В зависимости от платформы, этот код может отличаться
+
+//        // Пример для Windows
+//        var process = System.Diagnostics.Process.Start("cmd", "/C wuauclt /detectnow /updatenow");
+
+//        // Пример для macOS
+//        // var process = System.Diagnostics.Process.Start("/usr/sbin/softwareupdate", "-l -a");
+
+//        // Ожидание завершения операции
+//        process.WaitForExit();
+
+//        // Получение кода возврата
+//        int exitCode = process.ExitCode;
+
+//        // Обработка результата и вывод информации
+//        if (exitCode == 0)
+//        {
+//            Console.WriteLine("Обновления операционной системы установлены.");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Установка обновлений операционной системы не удалась.");
+//        }
+
+//        Console.WriteLine("Проверка и установка обновлений операционной системы завершена.");
+//    }
+
+//    private static void CheckAndInstallAppUpdates()
+//    {
+//        Console.WriteLine("Проверка наличия и установка обновлений приложений...");
+
+//        // Код для проверки наличия и установки обновлений приложений
+//        // В зависимости от приложений, этот код может отличаться
+
+//        // Пример для .NET приложений, использующих NuGet
+//        var process = System.Diagnostics.Process.Start("dotnet", "restore");
+
+//        // Ожидание завершения операции
+//        process.WaitForExit();
+
+//        // Получение кода возврата
+//        int exitCode = process.ExitCode;
+
+//        // Обработка результата и вывод информации
+//        if (exitCode == 0)
+//        {
+//            Console.WriteLine("Обновления приложений установлены.");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Установка обновлений приложений не удалась.");
+//        }
+
+//        Console.WriteLine("Проверка и установка обновлений приложений завершена.");
+//    }
+//}
